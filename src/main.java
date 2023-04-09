6b93bb9895e4fce783dc16fc57d8935f8aad9300
joanne5548 <91037706+joanne5548@users.noreply.github.com>
2023-04-08 21:02:39 -0400

    task1 update

--- src/main.java
+++ src/main.java
@@ -86,7 +86,7 @@ class Main {
 
         // Print out solution set, each line is "Stock BuyDay SellDay"
         for(int i = 0; i < result.length; i++) {
-            System.out.println(result[i][0] + " " + result[i][1] + result[i][2]);
+            System.out.println(result[i][0] + " " + result[i][1] + " " + result[i][2]);
         }
     }
 }
--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -4,7 +4,33 @@ public class Task1 {
 
     public static int[] solve(int[][] data) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'solve'");
+
+        int maxStock = 0, maxBuy = 0, maxSell = 0, maxPrice = 0;
+
+        for (int stock = 0; stock < data.length; ++stock)
+        {
+            for (int buy = 0; buy < data[0].length - 1; ++buy)
+            {
+                // Start from the same day you bought in case where prices only goes down
+                for (int sell = buy; sell < data[0].length; ++sell)
+                {
+                    int price = data[stock][sell] - data[stock][buy];
+                    if (price > maxPrice)
+                    {
+                        maxPrice = price;
+                        maxStock = stock;
+                        maxBuy = buy;
+                        maxSell = sell;
+                    }
+                }
+            }
+        }
+
+        int[] answer = {maxStock, maxBuy, maxSell};
+        return answer;
+
+
+        //throw new UnsupportedOperationException("Unimplemented method 'solve'");
     }
 
 }
