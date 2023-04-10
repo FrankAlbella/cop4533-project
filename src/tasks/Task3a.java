a30e377d38c9ea64735fd39d45a50f76a256449a
FrankAlbella <frankalbella@gmail.com>
2023-04-09 20:02:21 -0400

    Add task3a and 3b solutions

--- src/tasks/Task3a.java
+++ src/tasks/Task3a.java
@@ -1,78 +1,55 @@
 package tasks;
 
+import java.util.Arrays;
+
 public class Task3a {
-    static class Transaction
-    {   
-        public int stock = -1;
-        public int buyDay = -1;
-        public int sellDay = -1;
-        public int profit = -1;
-    };
 
     public static int[] solve(int[][] data) {
-        Transaction bestTrans = solveTail(data, 0);
-        return new int[] {bestTrans.stock, bestTrans.buyDay, bestTrans.sellDay};
-    }
+        int[] result = new int[3];
+        int[][] memo = new int[data.length][data[0].length];
 
-    static Transaction solveTail(int[][] data, int stock) {
-        // If out of range than return default values of -1
-        if(stock >= data.length) 
-            return new Transaction();
-        
-        Transaction localTrans = bestTransactionOnDay(data[stock]);
-        localTrans.stock = stock;
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], -1);
+        }
 
-        Transaction nextDayTrans = solveTail(data, stock+1);
+        int maxProfit = -1;
 
-        if (localTrans.profit < nextDayTrans.profit)
-            return nextDayTrans;
+        for (int stock = 0; stock < data.length; stock++) {
+            int minPrice = data[stock][0];
+            int minPriceDay = 0;
 
-        return localTrans;
-    }
+            for (int day = 1; day < data[stock].length; day++) {
+                if (data[stock][day] < minPrice) {
+                    minPrice = data[stock][day];
+                    minPriceDay = day;
+                }
 
-    static Transaction bestTransactionOnDay(int[] prices) {
-        Transaction trans = new Transaction();
-        int localMin = -1;
-        int localMax = -1;
-        for (int date = 1; date < prices.length; ++date)
-        {
-            // If we haven't had local minimum
-            if (localMin == -1)
-            {
-                // And if there is upward trend, update local minimum
-                if (prices[date] > prices[date - 1])
-                    localMin = date - 1;
-            }
-            // If we are in upward trend
-            // Check twice for local max at the end of the list
-            if (localMin != -1)
-            {
-                // And if there is price drop ahead OR we are at the end of the list
-                // If last two prices are the same and are local max, it returns the last index instead of first
-                if (prices[date - 1] > prices[date])
-                    localMax = date - 1;
-                else if (date == prices.length - 1)
-                    localMax = date;
-                
-                // If we are done with upward trend
-                if (localMax != -1)
-                {
-                    // And if current local price is higher than stored one
-                    if (prices[localMax] - prices[localMin] > trans.profit)
-                    {
-                        // Update local price, buy, sell
-                        trans.profit = prices[localMax] - prices[localMin];
-                        trans.buyDay = localMin;
-                        trans.sellDay = localMax;
-                    }
-                    // Reset localMin and max to search for another upward trend
-                    localMin = -1;
-                    localMax = -1;
+                memo[stock][day] = solveTail(data, memo, stock, day, minPrice);
+
+                if (memo[stock][day] > maxProfit) {
+                    maxProfit = memo[stock][day];
+                    result[0] = stock;
+                    result[1] = minPriceDay;
+                    result[2] = day;
                 }
             }
         }
-
-        return trans;
+        
+        return result;
     }
 
+    private static int solveTail(int[][] data, int[][] memo, int stock, int day, int minPrice) {
+        if (day == 0 || stock >= data.length)
+            return 0;
+
+        if (memo[stock][day] != -1)
+            return memo[stock][day];
+
+        int profitBuying = data[stock][day] - minPrice + solveTail(data, memo, stock, day - 1, minPrice);
+        int profitNotBuying = solveTail(data, memo, stock, day - 1, minPrice);
+
+        memo[stock][day] = Math.max(profitBuying, profitNotBuying);
+
+        return memo[stock][day];
+    }
 }
--- src/tasks/Task3b.java
+++ src/tasks/Task3b.java
@@ -1,10 +1,40 @@
 package tasks;
 
+import java.util.Arrays;
+
 public class Task3b {
 
     public static int[] solve(int[][] data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'solve'");
-    }
+        int[] result = new int[3];
+        int[][] memo = new int[data.length][data[0].length];
 
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], 0);
+        }
+
+        int maxProfit = -1;
+
+        for (int stock = 0; stock < data.length; stock++) {
+            int minPrice = data[stock][0];
+            int minPriceDay = 0;
+
+            for (int day = 1; day < data[stock].length; day++) {
+                if (data[stock][day] < minPrice) {
+                    minPrice = data[stock][day];
+                    minPriceDay = day;
+                }
+
+                memo[stock][day] = Math.max(memo[stock][day - 1], data[stock][day] - minPrice);
+
+                if (memo[stock][day] > maxProfit) {
+                    maxProfit = memo[stock][day];
+                    result[0] = stock;
+                    result[1] = minPriceDay;
+                    result[2] = day;
+                }
+            }
+        }
+
+        return result;
+    }
 }
