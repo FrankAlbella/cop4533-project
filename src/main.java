f298c593d4da83732820d3985f1cead434263734
joanne5548 <91037706+joanne5548@users.noreply.github.com>
2023-04-10 00:25:13 -0400

    +1 on index

--- src/main.java
+++ src/main.java
@@ -87,7 +87,7 @@ class Main {
 
         // Print out solution set, each line is "Stock BuyDay SellDay"
         for(int i = 0; i < result.length; i++) {
-            System.out.println(result[i][0] + " " + result[i][1] + " " + result[i][2]);
+            System.out.println((result[i][0] + 1)+ " " + (result[i][1] + 1) + " " + (result[i][2] + 1));
         }
     }
 }
