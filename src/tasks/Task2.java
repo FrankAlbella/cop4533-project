dad8977e53035030384f554e70815362609f5379
joanne5548 <91037706+joanne5548@users.noreply.github.com>
2023-04-09 23:53:36 -0400

    fixed task 2

--- src/tasks/Task2.java
+++ src/tasks/Task2.java
@@ -7,42 +7,63 @@ public class Task2 {
         int localPrice = -1, localBuy = -1, localSell = -1;
         for (int stock = 0; stock < data.length; ++stock)
         {
-            int localMin = -1;
-            int localMax = -1;
+            int localMin = 0;
+            int localMax = 0;
+            boolean up = false;
             for (int date = 1; date < data[0].length; ++date)
             {
-                // If we haven't had local minimum
-                if (localMin == -1)
-                {
-                    // And if there is upward trend, update local minimum
-                    if (data[stock][date] > data[stock][date - 1])
-                        localMin = date - 1;
-                }
                 // If we are in upward trend
-                // Check twice for local max at the end of the list
-                if (localMin != -1)
+                if (data[stock][date] > data[stock][date - 1])
+                {
+                    // And this is the first instance,
+                    // And if stock price is lower than or equal to (first instance) the previous local Min,
+                    // Update local min
+                    if (!up)
+                    {
+                        if (data[stock][date-1] <= data[stock][localMin])
+                            localMin = date - 1;
+                        up = true;
+                    }
+                }
+            
+                // If we are in upward trend
+                if (up)
                 {
                     // And if there is price drop ahead OR we are at the end of the list
                     // If last two prices are the same and are local max, it returns the last index instead of first
                     if (data[stock][date - 1] > data[stock][date])
-                        localMax = date - 1;
-                    else if (date == data[0].length - 1)
-                        localMax = date;
-                    
-                    // If we are done with upward trend
-                    if (localMax != -1)
                     {
+                        localMax = date - 1;
+                        up = false;
+                    }
+                    else if (date == data[0].length - 1)
+                    {
+                        localMax = date;
+                        up = false;
+                    }
+
+                    // If we are done with upward trend
+                    if (!up)
+                    {
+                        int currentLocalPrice = data[stock][localMax] - data[stock][localMin];
                         // And if current local price is higher than stored one
-                        if (data[stock][localMax] - data[stock][localMin] > localPrice)
+                        if (currentLocalPrice > localPrice)
                         {
-                            // Update local price, buy, sell
-                            localPrice = data[stock][localMax] - data[stock][localMin];
-                            localBuy = localMin;
+                            localPrice = currentLocalPrice;
                             localSell = localMax;
+
+                            // If previous local min DOES make higher profit than current local min
+                            // And is NOT first instance of local Buy
+                            if (localBuy != -1 && currentLocalPrice < data[stock][localMax] - data[stock][localBuy])
+                            {
+                                // only update local Price and Sell
+                                localPrice = data[stock][localMax] - data[stock][localBuy];
+                                // Keep local Buy, don't update
+                                localSell = localMax;
+                            }
+                            else
+                                localBuy = localMin;
                         }
-                        // Reset localMin and max to search for another upward trend
-                        localMin = -1;
-                        localMax = -1;
                     }
                 }
             }
